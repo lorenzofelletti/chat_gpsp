af03b4033cafa7ea7ab521f8d88452ad6c1933ca
Lorenzo Felletti <lorenzo97.felletti@gmail.com>
2024-03-13 22:02:00 +0000

    feat: add traits for DNS resolver

--- src/net/mod.rs
+++ src/net/mod.rs
@@ -1,4 +1,5 @@
 pub mod dns;
 pub mod netc;
 pub mod socket;
+pub mod traits;
 pub mod utils;
---
+++ src/net/traits/dns.rs
@@ -0,0 +1,18 @@
+use core::fmt::Debug;
+
+use alloc::string::String;
+
+use psp::sys::in_addr;
+
+// trait defining a type that can resolve a hostname to an IP address
+pub trait ResolveHostname {
+    type Error: Debug;
+    fn resolve_hostname(&mut self, hostname: &str) -> Result<in_addr, Self::Error>;
+}
+
+pub trait ResolveAddr {
+    type Error: Debug;
+    fn resolve_addr(&mut self, addr: in_addr) -> Result<String, Self::Error>;
+}
+
+pub trait DnsResolver: ResolveHostname + ResolveAddr {}
